683aa957ba868bb2dbadc806299c226eae701edf
swilson304 <swilson304@gatech.edu>
2020-09-24 19:38:22 -0400

    Make GUI compatible with Player and Plot classes

--- src/main/InitialFarmUISebastian.java
+++ src/main/InitialFarmUISebastian.java
@@ -24,15 +24,16 @@ public class InitialFarmUISebastian extends Application {
         int difficulty = 0; //THIS VARIABLE WILL EVENTUALLY BE SET BY THE USER FROM THE INITIAL CONFIG SCREEN.
         Date date = new Date(2020, 1, 1);
         Player player = new Player(difficulty);
-        double startingMoney = Player.getMoney();
+        double startingMoney = player.getMoney();
         VBox vbox = new VBox();
         vbox.setMaxSize(800, 800);
         Label startingMoneyLabel = new Label("Starting money: $" + startingMoney);
         vbox.getChildren().add(startingMoneyLabel);
         Label displayDateLabel = new Label("Today's date: " + date.toString());
-        Plot[] plots = Player.getFarm().getPlots();
+        Plot[] plots = player.getFarm().getPlots();
         for (int i = 0; i < plots.length; i++) {
-            vbox.getChildren().add(plots[i]);
+            vbox.getChildren().add(new Label("Plot " + i + 1));
+            vbox.getChildren().add(new Label("Title: " + plots[i].getTitle() + " Type: " + plots[i].getType() + " Number of Crops" + plots[i].getNumCrops()));
         }
         Scene scene = new Scene(vbox, 800, 700);
         primaryStage.setTitle("FARM GAME");
