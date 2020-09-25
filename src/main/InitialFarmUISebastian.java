a08b9918fdd298ae0d9e0569243dcfb679c9f8f0
swilson304 <swilson304@gatech.edu>
2020-09-24 20:04:32 -0400

    Fix bugs

--- src/main/InitialFarmUISebastian.java
+++ src/main/InitialFarmUISebastian.java
@@ -1,39 +1,30 @@
 package main;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontPosture;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
-import java.util.Date;
 
 public class InitialFarmUISebastian extends Application {
 
     @Override
     public void start(Stage primaryStage) {
         int difficulty = 0; //THIS VARIABLE WILL EVENTUALLY BE SET BY THE USER FROM THE INITIAL CONFIG SCREEN.
-        Date date = new Date(2020, 1, 1);
         Player player = new Player(difficulty);
         double startingMoney = player.getMoney();
         VBox vbox = new VBox();
         vbox.setMaxSize(800, 800);
         Label startingMoneyLabel = new Label("Starting money: $" + startingMoney);
         vbox.getChildren().add(startingMoneyLabel);
-        Label displayDateLabel = new Label("Today's date: " + date.toString());
+        Label displayDateLabel = new Label("Today's date: Day 1");
+        vbox.getChildren().add(displayDateLabel);
         Plot[] plots = player.getFarm().getPlots();
+        int plotNumber;
         for (int i = 0; i < plots.length; i++) {
-            vbox.getChildren().add(new Label("Plot " + i + 1));
-            vbox.getChildren().add(new Label("Title: " + plots[i].getTitle() + " Type: " + plots[i].getType() + " Number of Crops" + plots[i].getNumCrops()));
+            plotNumber = i + 1;
+            vbox.getChildren().add(new Label("Plot " + plotNumber));
+            vbox.getChildren().add(new Label("Title: " + plots[i].getTitle() + " Type: " + plots[i].getType() + " Number of Crops " + plots[i].getNumCrops()));
         }
         Scene scene = new Scene(vbox, 800, 700);
         primaryStage.setTitle("FARM GAME");
