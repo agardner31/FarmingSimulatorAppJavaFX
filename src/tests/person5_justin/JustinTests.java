f5ecf232164c5ac74cee33c54e72865e893b90a2
jcawthon3 <jcawthon3@gatech.edu>
2020-09-27 12:43:51 -0400

    Added test file for intilization of farm and plot

---
+++ src/tests/person5_justin/JustinTests.java
@@ -0,0 +1,53 @@
+package tests.person5_justin;
+
+import main.Farm;
+import main.Plot;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class JustinTests {
+
+    private Farm farm;
+    private Plot plot;
+
+    @Before
+    public void setUp()  {
+        farm = new Farm();
+    }
+
+    @Test
+    public void testPlotArrayInitialization() {
+        Plot[] plots = new Plot[10];
+        for (int i = 0; i < 10; i++) {
+            plots[i] = new Plot();
+        }
+        for (int i = 0; i < 10; i++) {
+            assertEquals(plots[i].getNumCrops(), 0);
+            assertEquals(plots[i].getType(), "None");
+            assertEquals(plots[i].getTitle(), "");
+        }
+    }
+
+    @Test
+    public void testFarmInitialization() {
+        int size = 4;
+        farm = new Farm(4);
+        Plot[] plots = new Plot[size];
+        for (int i = 0; i < size; i++) {
+            plots[i] = new Plot();
+        }
+        for (int i = 0; i < size; i++) {
+            assertEquals(farm.getPlots()[i].getNumCrops(), 0);
+            assertEquals(farm.getPlots()[i].getType(), "None");
+            assertEquals(farm.getPlots()[i].getTitle(), "Plot 1");
+            assertEquals(plots[i].getNumCrops(), 0);
+            assertEquals(plots[i].getType(), "None");
+            assertEquals(plots[i].getTitle(), "");
+            assertEquals(farm.getPlots()[i].getNumCrops(), plots[i].getNumCrops());
+            assertEquals(farm.getPlots()[i].getType(), plots[i].getType());
+            assertEquals(farm.getPlots()[i].getTitle(), plots[i].getTitle());
+        }
+    }
+}
