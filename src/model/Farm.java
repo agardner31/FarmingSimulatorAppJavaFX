3b9d8d4f2744e71bd35af002488b05f89e374070
bburns39 <bburns39@gatech.edu>
2020-10-25 14:12:09 -0400

    Added BenM4Test

--- src/model/Farm.java
+++ src/model/Farm.java
@@ -1,7 +1,5 @@
 package model;
 
-import java.util.Random;
-
 public class Farm {
     private Plot[] plots;
 
@@ -9,8 +7,6 @@ public class Farm {
 
     public Farm(String difficulty) {
         plots = new Plot[initialCapacity];
-//        String[] randomType = new String[]{"Pumpkin", "Tomato", "Corn"};
-//        Random rand = new Random();
         for (int i = 0; i < plots.length; i++) {
             plots[i] = new Plot(null, i + 1);
         }
@@ -18,11 +14,8 @@ public class Farm {
 
     public Farm(int size, String difficulty) {
         plots = new Plot[size];
-        String[] randomType = new String[]{"Pumpkin", "Tomato", "Corn"};
-        Random rand = new Random();
         for (int i = 0; i < plots.length; i++) {
-            plots[i] = new Plot(new Crop(randomType[rand.nextInt(3)],
-                    difficulty), i + 1);
+            plots[i] = new Plot(null, i + 1);
         }
     }
 
---
+++ src/tests/M4/BenM4Test.java
@@ -0,0 +1,63 @@
+package tests.M4;
+import model.Crop;
+import model.CropStage;
+import model.Player;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class BenM4Test {
+    private Player player;
+    private Crop testCrop;
+
+    @Before
+    public void setup() {
+        player = new Player();
+        testCrop = new Crop("Corn", player.getDifficulty());
+    }
+
+    @Test
+    public void dayIncrementTestNoWater() {
+        assertEquals(player.getDay(), 1);
+        assertEquals(testCrop.getStage(), CropStage.SEED);
+
+        hitNextDayButton();
+
+        assertEquals(player.getDay(), 2);
+        assertEquals(testCrop.getStage(), CropStage.IMMATURE);
+
+        hitNextDayButton();
+
+        assertEquals(player.getDay(), 3);
+        assertEquals(testCrop.getStage(), CropStage.DEAD);
+    }
+
+    @Test
+    public void dayIncrementTestWater() {
+        assertEquals(player.getDay(), 1);
+        assertEquals(testCrop.getStage(), CropStage.SEED);
+
+        testCrop.water();
+        hitNextDayButton();
+
+        assertEquals(player.getDay(), 2);
+        assertEquals(testCrop.getStage(), CropStage.IMMATURE);
+
+        testCrop.water();
+        hitNextDayButton();
+
+        assertEquals(player.getDay(), 3);
+        assertEquals(testCrop.getStage(), CropStage.MATURE);
+
+        testCrop.water();
+        hitNextDayButton();
+
+        assertEquals(player.getDay(), 4);
+        assertEquals(testCrop.getStage(), CropStage.DEAD);
+    }
+
+    private void hitNextDayButton() {
+        player.incrementDay();
+        testCrop.grow();
+    }
+}
