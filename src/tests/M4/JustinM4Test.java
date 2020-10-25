4cc7cf371ef01d2b4cbbd34b381c5c763b6e3678
jcawthon3 <jcawthon3@gatech.edu>
2020-10-25 13:10:38 -0400

    added test

---
+++ src/tests/M4/JustinM4Test.java
@@ -0,0 +1,12 @@
+package tests.M4;
+import model.Crop;
+import model.CropStage;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class JustinM4Test {
+    @Test
+    public void test() {
+
+    }
+}
